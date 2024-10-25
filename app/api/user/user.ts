b655bbcc162b589a0001629833dd4424e3b3f096
ehdrbdndns <ehdrbdndns@naver.com>
2024-10-25 22:52:56 +0900

    feat: add user query function

--- app/api/user/user.ts
+++ app/api/user/user.ts
@@ -9,10 +9,28 @@ async function getAllUsers() {
       , oauth_email, oauth_provider
     FROM user
 
-    LEFT JOIN user_state ON user.user_id = user_state.user_id
+    LEFT JOIN user_state 
+      
+      ON user.user_id = user_state.user_id
   `);
 
   return rows;
 }
 
-export { getAllUsers }
+async function insertOrUpdateUserState(user_id: string, role: string, status: string) {
+  // const [result, fields] = await promisePool.query(`
+  //   INSERT INTO user_state (role, status)
+
+  //     VALUES (?, ?)
+
+  //   ON DUPLICATE KEY UPDATE
+  //     role = VALUES(role),
+  //     status = VALUES(status)
+
+  //   WHERE user_id = ?
+  // `, [role, status, user_id]);
+
+  // console.log(result, fields);
+}
+
+export { getAllUsers, insertOrUpdateUserState }
