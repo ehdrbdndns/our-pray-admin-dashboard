7425965e611f9250faeb164aa75e646e387f0e0c
ehdrbdndns <ehdrbdndns@naver.com>
2024-10-29 01:06:44 +0900

    fix: user router

--- app/(dashboard)/users/user-status-modal.tsx
+++ app/(dashboard)/users/user-status-modal.tsx
@@ -39,12 +39,14 @@ export default function UserStatusModal({ user }: { user: UserType }) {
   const updateUserState = async () => {
     setIsLoading(true);
 
-    console.log(process.env.API_URL);
-
     try {
-      // fetch
       await fetch(`/api/user/status`, {
         method: 'POST',
+        body: JSON.stringify({
+          user_id: user.user_id,
+          role,
+          status
+        }),
       });
     } catch (e) {
       console.error(e);
@@ -57,11 +59,11 @@ export default function UserStatusModal({ user }: { user: UserType }) {
   const onClickUpdateButton = async () => {
     try {
       await updateUserState();
+      alert('사용자 상태가 변경되었습니다.');
     } catch (e) {
       alert(e);
     }
 
-    alert('사용자 상태가 변경되었습니다.');
     triggerBtn.current?.click();
   }
 
