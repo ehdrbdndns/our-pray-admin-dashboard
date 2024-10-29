bcd231e54e9b8eb12d6b4d7487c335374a240340
ehdrbdndns <ehdrbdndns@naver.com>
2024-10-29 16:00:37 +0900

    feat: add get full date function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getFullDateFromDate(date: Date) {
+  return `${date.getFullYear()} - ${date.getMonth()} - ${date.getDate()}`
+}
