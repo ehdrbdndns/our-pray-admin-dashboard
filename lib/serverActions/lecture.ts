d375a924e90ebe04f432bd04ff2ae7daf0653f7e
ehdrbdndns <ehdrbdndns@naver.com>
2024-11-09 15:56:12 +0900

    feat: add retrieve lecture data function

---
+++ lib/serverActions/lecture.ts
@@ -0,0 +1,18 @@
+import promisePool from "../db/db";
+import { LectureType } from "../db/type";
+
+export const retrievelectureByLectureId = async (lecture_id: string) => {
+  try {
+    const [rows] = await promisePool.query<LectureType[]>(`
+      SELECT * 
+      FROM lecture 
+      WHERE lecture_id = ?
+      LIMIT 1
+    `, [lecture_id]);
+
+    return rows[0];
+  } catch (e) {
+    console.error(e);
+    throw new Error('강의 정보를 가져오는데 실패했습니다.');
+  }
+}
