ae734e36a651dd146d6c3cd58d93a9030260724a
ehdrbdndns <ehdrbdndns@naver.com>
2024-11-09 16:03:40 +0900

    fix: 쿼리문 수정

--- lib/serverActions/lecture.ts
+++ lib/serverActions/lecture.ts
@@ -1,16 +1,15 @@
 import promisePool from "../db/db";
 import { LectureType } from "../db/type";
 
-export const retrieveLectureByLectureId = async (lecture_id: string) => {
+export const retrieveLecturesByPlanId = async (plan_id: string) => {
   try {
     const [rows] = await promisePool.query<LectureType[]>(`
       SELECT * 
       FROM lecture 
-      WHERE lecture_id = ?
-      LIMIT 1
-    `, [lecture_id]);
+      WHERE plan_id = ?
+    `, [plan_id]);
 
-    return rows[0];
+    return rows;
   } catch (e) {
     console.error(e);
     throw new Error('강의 정보를 가져오는데 실패했습니다.');
